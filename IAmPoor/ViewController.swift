51d763504122edec9272a262178d68fa0067b79c
TristanAly <tristan.aly@gmail.com>
2022-08-04 09:33:06 +0200

    Initial Commit

---
+++ IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Tristan Aly on 04/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
